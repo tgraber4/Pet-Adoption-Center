f129b5c39977fe99719bdc52eecb1007bd352457
tgraber4 <tgraber4@gmail.com>
2025-05-02 16:48:03 -0500

    init project

---
+++ src/main/java/org/group25/PetAdoptionCenter/App.java
@@ -0,0 +1,17 @@
+package org.group25.PetAdoptionCenter;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Gson gson = new Gson();
+    }
+}
